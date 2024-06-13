b928078acef1f98b7a06aece75d6f41d0aa3ef82
aiyuduernest <aiyuduernest@gmail.com>
2024-06-13 10:34:14 -0700

    Initial commit

---
+++ javascriptlab6.js
@@ -0,0 +1,40 @@
+// Task 1: Variables
+
+// Declare variables
+var myNumber = 10;
+var myString = "Hello, JavaScript!";
+
+// Printing variables to the console
+
+console.log(myNumber);
+console.log(myString);
+
+// Task 2: Loops
+
+// For loop from 0 to 10
+for (var i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+// For loop calculating and to print the first 5 multiples of 3
+for (var m = 1; m <= 5; m++) {
+    console.log(3 * m);
+}
+
+// Task 3: Functions
+
+// addNumber from functions 
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+// Call addNumbers and then print result
+console.log(addNumbers(3, 5));
+
+// Function to multiply two numbers
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+
+// Call multiplyNumbers and then print the result
+console.log(multiplyNumbers(3, 7));
